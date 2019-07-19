71e1f8d4b0d0bf337fe9d19bb90ccb26ab20c07d
Vũ Tùng Dương <duongvutung1998@gmail.com>
2019-07-19 10:23:16 +0700

    create modules and components

--- BookSystem/AngularClientApp/src/app/app.module.ts
+++ BookSystem/AngularClientApp/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BooksystemModule } from "../booksystem/booksystem.module";
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
+import { UserModule } from './user/user.module';
+import { BookModule } from './book/book.module';
+import { AdminModule } from './admin/admin.module';
+import { TemplateModule } from './template/template.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BooksystemModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        UserModule,
+        AdminModule,
+        BookModule,
+        TemplateModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ BookSystem/AngularClientApp/src/app/book/detail/detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'book-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.css']
+})
+export class BookDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
