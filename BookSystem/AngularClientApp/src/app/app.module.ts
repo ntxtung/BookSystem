dd7a41f418a37657aa441a897e791009a56829a5
Vũ Tùng Dương <duongvutung1998@gmail.com>
2019-07-19 10:50:43 +0700

    create api services

--- BookSystem/AngularClientApp/src/app/app.module.ts
+++ BookSystem/AngularClientApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
+
 import { UserModule } from './user/user.module';
 import { BookModule } from './book/book.module';
 import { AdminModule } from './admin/admin.module';
 import { TemplateModule } from './template/template.module';
+
+import { UserApiService } from './services/user-services/user-api-service.service';
+import { BookApiService } from './services/book-services/book-api-service.service';
 
 @NgModule({
     declarations: [
@@ -19,7 +23,10 @@ import { TemplateModule } from './template/template.module';
         BookModule,
         TemplateModule
     ],
-    providers: [],
+    providers: [
+        UserApiService,
+        BookApiService
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
