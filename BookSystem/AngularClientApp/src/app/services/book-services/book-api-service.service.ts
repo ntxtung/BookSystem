19349a20183fec8ed619948bf4b38b81c9780577
Vũ Tùng Dương <duongvutung1998@gmail.com>
2019-07-24 16:16:38 +0700

    list all books

--- BookSystem/AngularClientApp/src/app/services/book-services/book-api-service.service.ts
+++ BookSystem/AngularClientApp/src/app/services/book-services/book-api-service.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class BookApiService {
+    private BASE_URL = "http://localhost:5000"
+    constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+    getBooks(){
+        return this.httpClient.get(`${this.BASE_URL}/api/books`);
+    }
 }
