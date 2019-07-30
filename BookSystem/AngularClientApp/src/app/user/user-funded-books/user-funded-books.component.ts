8fbfdf89096198a485d7eaf87016393552aad3c8
Vũ Tùng Dương <duongvutung1998@gmail.com>
2019-07-30 12:42:39 +0700

    fix some authorizing code

--- BookSystem/AngularClientApp/src/app/user/user-funded-books/user-funded-books.component.ts
+++ BookSystem/AngularClientApp/src/app/user/user-funded-books/user-funded-books.component.ts
@@ -3,6 +3,7 @@ import { BookApiService } from 'src/app/services/book-services/book-api-service.
 import { Observable } from 'rxjs';
 import { Book } from 'src/app/models/book.model';
 import { User } from 'src/app/models/user.model';
+import { Store, select } from '@ngrx/store';
 
 @Component({
     selector: 'user-funded-books',
@@ -13,10 +14,16 @@ export class UserFundedBooksComponent implements OnInit {
 
     books: Book[]
     user: User
-    constructor(private bookApiService: BookApiService) { }
+    constructor(private bookApiService: BookApiService, private store: Store<any>) { }
 
     ngOnInit() {
-        this.user = JSON.parse(localStorage.getItem("user"));
+        this.store.pipe(select("users")).subscribe(
+            users => {
+                if(users){
+                    this.user = users.user
+                }
+            }
+        )
         this.bookApiService.getUserFundedBooks(this.user).subscribe(
             books => {
                 this.books = books;
