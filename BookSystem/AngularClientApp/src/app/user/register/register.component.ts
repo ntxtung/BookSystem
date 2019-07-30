06c1b4f754f5fd6ffe8ccce58ab0c81d22e5d5de
Vũ Tùng Dương <duongvutung1998@gmail.com>
2019-07-30 13:49:06 +0700

    some stupid changes

--- BookSystem/AngularClientApp/src/app/user/register/register.component.ts
+++ BookSystem/AngularClientApp/src/app/user/register/register.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+    loginRegisterData: any = {
+        email: null,
+        username: null,
+        password: null,
+        repassword: null,
+    }
+    passwordMatching: boolean = true
     constructor() { }
 
     ngOnInit() {
+    }
+
+    checkConfirmPassword(){
+        if(this.loginRegisterData.password != this.loginRegisterData.repassword){
+            this.passwordMatching = false;
+        }
+        else {
+            this.passwordMatching = true;
+        }
+        console.log("passwordMatching: "+this.passwordMatching)
     }
 
 }
